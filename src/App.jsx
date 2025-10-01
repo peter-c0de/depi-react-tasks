c97db6c99e7575158c40f3b5f8d90794d090514b
Peter c0de <pet3r.code@gmail.com>
2025-10-01 21:22:48 +0300

    React: Session 01

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Hello React.js</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis maiores unde quidem, ea eius reprehenderit ut a odio eum asperiores, aliquam obcaecati sequi cupiditate adipisci, porro aliquid accusamus laudantium neque!</p>
+    </div>
+  )
+}
+
+export default App
