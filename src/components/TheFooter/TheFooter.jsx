e083435951a9a2a6dfcc70422696363f52bb2b51
Peter c0de <pet3r.code@gmail.com>
2025-10-19 16:58:03 +0300

    Lec4: Commit #12

--- src/components/TheFooter/TheFooter.jsx
+++ src/components/TheFooter/TheFooter.jsx
@@ -1,9 +1,12 @@
 import { Container } from 'react-bootstrap';
-import './TheFooter.css'
+
+// import './TheFooter.css'
+import classes from './TheFooter.module.css'
 
 function TheFooter(){
     return (
-        <footer className="hamada">
+        // <footer className="hamada">
+        <footer className={classes.hamada}>
             <Container>
                 <p className="text-center">react project copyrights</p>
             </Container>
